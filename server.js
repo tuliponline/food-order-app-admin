187ce8772d5e742efd28468ac6c88bc932722fd9
@apicac70 <50069747+tuliponline@users.noreply.github.com>
2025-04-11 12:27:43 +0700

    change to lagacy

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const hostname = 'localhost';
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, (err) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:' + (process.env.PORT || 3000));
+    console.log(`> Ready on http://${hostname}:${port}`);
   });
 }); 
