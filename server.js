3f6242252942ec9b473825757d2d093f07f62469
@apicac70 <50069747+tuliponline@users.noreply.github.com>
2025-04-11 13:41:35 +0700

    Revert "change to lagacy"
    This reverts commit 187ce8772d5e742efd28468ac6c88bc932722fd9.

--- server.js
+++ server.js
@@ -3,24 +3,15 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = process.env.PORT || 3000;
-
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      handle(req, res, parsedUrl);
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err);
-      res.statusCode = 500;
-      res.end('Internal Server Error');
-    }
-  }).listen(port, (err) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log('> Ready on http://localhost:' + (process.env.PORT || 3000));
   });
 }); 
